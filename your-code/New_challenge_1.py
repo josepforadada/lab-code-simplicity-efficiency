3a4a9f9e17a2e8ffa460b8e338b948489f6c108a
Josep <workwithjosep@gmail.com>
2019-07-09 15:29:52 +0200

    Rename files

---
+++ your-code/New_challenge_1.py
@@ -0,0 +1,28 @@
+"""
+This is a dumb calculator that can add and subtract whole numbers from zero to five.
+When you run the code, you are prompted to enter two numbers (in the form of English
+word instead of number) and the operator sign (also in the form of English word).
+The code will perform the calculation and give the result if your input is what it
+expects.
+
+The code is very long and messy. Refactor it according to what you have learned about
+code simplicity and efficiency.
+"""
+
+# screen return
+
+print('Welcome to this calculator!')
+print('It can add and subtract whole numbers from zero to five')
+
+# screen inputs
+
+a = input('Please choose your first number (zero to five): ')
+b = input('What do you want to do? plus or minus: ')
+c = input('Please choose your second number (zero to five): ')
+
+
+
+if (not a == 'zero' and not a == 'one' and not a == 'two' and not a == 'three' and not a == 'four' and not a == 'five') or (not c == 'zero' and not c == 'one' and not c == 'two' and not c == 'three' and not c == 'four' and not c == 'five') or (not b == 'plus' and not b == 'minus'):
+    print("I am not able to answer this question. Check your input.")
+
+print("Thanks for using this calculator, goodbye :)")
